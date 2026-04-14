f932ace5e7e0438a0b5a43197b837d4274a7b420
Trc300online <trieracolomar@gmail.com>
2026-04-14 13:46:12 +0200

    working on game logic

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,31 @@
-import java.util.ArrayList;
-
 public class Game {
 
-    public ArrayList<Player> playerList = new ArrayList<>();
+    public Player[] playerList = new Player[2];
 
     public void playGame() {
 
-        playerList.add(new Player(Screen.askPlayerName(), new Board()));
-        playerList.add(new Player(Screen.askPlayerName(), new Board()));
+        boolean gameContinue = true;
+        int turnCounter = 2;
+        playerList[0] = new Player(Screen.askPlayerName(), new Board());
+        playerList[1] = new Player(Screen.askPlayerName(), new Board());
+
+        while (gameContinue) {
+            Screen.printPublicBoard(playerList[(turnCounter+1)%2].getBoard());
+            Screen.printPrivateBoard(playerList[turnCounter%2].getBoard());
+            //player action
+            int[] coords = getAction();
+            doAction(coords);
+            //gameContinue = checkWin(playerList[(turnCounter+1)%2].getBoard()) --> this methode checks if rival board has all ship sunk/shipParts hitted.
+        }
+    }
+
+    public int[] getAction() {
+        
     }
 }
+
+
+/*
+playerList[turnCounter%2] --> current player
+playerList[(turnCounter+1)%2]  --> other player
+*/
